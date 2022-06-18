2c84241ae2e921c34147b214eb8ec6e2407d9b6a
Manuel Galindo <mfdogalindo@outlook.com>
2022-06-18 13:04:56 -0500

    Eventos de teclado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
-  title = 'Calculator';
-  result = 0;;
+  pantalla = 0;
 
-  sum() {
-    this.result++
-    console.log(this.result)
+  sumar(): void {
+    this.pantalla++;
+    console.log(this.pantalla);
   }
 
-  reset(){
-    this.result = 0;
+  restar(): void {}
+
+  multiplicar(): void {}
+
+  dividir(): void {}
+
+  reiniciar(): void {
+    this.pantalla = 0;
+  }
+
+  // Eventos del teclado
+  @HostListener('window:keyup', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    console.log(event);
+    if (event.key == 'Enter') {
+      console.log('Es un enter');
+    }
+    if (event.key == 'Escape') {
+      this.reiniciar();
+    }
+    if (event.key == 'Backspace') {
+      console.log('Es un backspace');
+      this.pantalla = parseInt(this.pantalla.toString().slice(0, -1));
+      if (isNaN(this.pantalla)) this.pantalla = 0;
+    }
+    if (event.key == 'Delete') {
+      this.reiniciar();
+    }
+    if (event.key.match(/^[0-9]$/)) {
+      this.pantalla = this.pantalla * 10 + parseInt(event.key);
+    }
   }
 }
